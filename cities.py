5bc1573a1c7a571f8b23e3e76b2cbb8aa1df2b55
JoeClarkMcGhee <joeclarkmcghee@yahoo.co.uk>
2019-12-01 18:38:59 +0000

    Use command line arguments to run the programme.

--- cities.py
+++ cities.py
@@ -1,24 +1,22 @@
+import argparse
 import os
 
 from application import cities_interface
 from application import visualize_road_map
 
 
-def main():
+def main(*, file_name="city-data.txt"):
     """
     The programme reads in and prints out the provided city data from the file, then creates the
     best cycle and prints it out.
 
     Usage:
-        - Provide the variable *file* with the file name of city data.
-        - The file must be in the pop-one-project-jclark42 directory, not one of the nested
+        - The program must be executed from the command line e.g. `python3 cities.py <file_name>`
+        - Type `cities.py -h` for complete instructions.
+        - The data file must be in the pop-one-project-jclark42 directory, not one of the nested
           subdirectories.
-        - You do not need to provide the full path just the name of the file.
-        - Once you have set the *file* variable you can run the program by calling `python3
-          cities.py`.
-        - You must run the program in the pop-one-project-jclark42 directory
     """
-    file = "city-data.txt"
+    file = file_name
     this_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(this_dir, file)
     print("---- Initial road map ----")
@@ -34,5 +32,25 @@ def main():
     )
 
 
-if __name__ == "__main__":
+help_text = (
+    "-------------------------------------------------------\n"
+    "Provide a file name (with extension as .txt) consisting\n"
+    "of city data and the best route between the cities will\n"
+    "be calculated."
+    "\n\nExample: `python3 cities.py your-city-data.txt`"
+    "\n\nIf no file is provided and the optional `default_data`\n"
+    "argument is passed to cities.py then the route will be\n"
+    "calculated based on the data in  the `city-data.txt` file\n"
+    "found in this directory."
+    "\n\nNOTE: The file to process has to be in this directory."
+    "\n-------------------------------------------------------"
+)
+parser = argparse.ArgumentParser(
+    description=help_text, formatter_class=argparse.RawTextHelpFormatter
+)
+parser.add_argument("file_name", type=str, help="file of city data to process")
+args = parser.parse_args()
+if args.file_name == "default_data":
     main()
+else:
+    main(file_name=args.file_name)
